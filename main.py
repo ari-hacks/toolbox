a413bdd2e739c74ed0a1ecd151dbcd4be8247780
ariela <ariela.haley65@gmail.com>
2020-08-02 03:25:46 -0700

    setup

--- main.py
+++ main.py
@@ -19,27 +19,28 @@ def compress_file(file):
 
 @cli.command("email-manager")
 @click.argument("email")
-def compress_file():
+def email_manager():
     """Email Manager"""
     pass
 
 
 @cli.command("url-checker")
 @click.argument("url")
-def compress_file(url):
+def url_checker(url):
     """Url Check For Spam"""
     pass
 
 @cli.command("password-generator")
-def compress_file():
+def pass_gen():
     """Generate Password"""
+    #Add args for diff lengths 
     generated_password = base64.b64encode(os.urandom(65)).decode('utf-8')
     clipboard.copy(generated_password)
     return print(generated_password)
 
 @cli.command("sys-stats")
 @click.argument("stats")
-def compress_file(stats):
+def sys_stats(stats):
     """Check system stats"""
     pass
 
